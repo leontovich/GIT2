0c63d1fc4b0c4a4c49bad1445d5586cc1c905051
Pavel T <Yourname@example.com>
2019-12-05 18:44:18 +0300

    add funcs.(h/cpp) fitcha1

---
+++ GIT2/GIT2/funcs.cpp
@@ -0,0 +1,11 @@
+#include "pch.h" 
+#include "funcs.h" 
+#include <cmath> 
+double Minus(double a, double b)
+{
+	return a - b;
+}
+double Umnoj(double a, double b)
+{
+	return a * b;
+}
---
+++ GIT2/GIT2/funcs.h
@@ -0,0 +1,5 @@
+#ifdef __FUNCS_H__
+#define __FUNCS_H__
+double Minus(double, double); 
+double Umnoj(double, double);
+#endif // __FUNCS_H__
